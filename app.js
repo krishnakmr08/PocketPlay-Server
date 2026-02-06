17c8a2ddf9362107de6c4f6bd0bcb1159852759a
Krishna Kumar <kkbhagat20@gmail.com>
2026-02-06 13:52:42 +0530

    complete app.js with express, socket.io, routes, and database setup

---
+++ app.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const connectDB = require("./config/connectDB");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+const authRouter = require("./routes/auth");
+const playRouter = require("./routes/play");
+
+const socketService = require("./controllers/sockets");
+
+const app = express();
+
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+socketService(io);
+
+app.use("/auth", authRouter);
+app.use("/play", playRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    server.listen(process.env.PORT || 3000, () =>
+      console.log(
+        `HTTP server is running on http://localhost:${process.env.PORT || 3000}`,
+      ),
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
